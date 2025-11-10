f304c3b3e8ec4fbecefc9754159fcd90a288b05d
Gani25 <memonabdulgani3d@gmail.com>
2025-11-10 10:56:03 +0530

    Update day13.sql

--- day13.sql
+++ day13.sql
@@ -41,3 +41,53 @@ natural join my_table_2;
 
 select * from my_table_1
 natural left join my_table_2;
+
+
+create table food_item
+(
+	name varchar(50),
+    price int
+);
+
+select * from food_item;
+insert into food_item values
+("Veg Burger",60),
+("Pizza",250),
+("Sandwich",80),
+("Pav Bhaji",100);
+
+
+create table variations
+(
+	toppings varchar(50),
+    extra_amount int
+);
+
+insert into variations values
+("Simple",0),
+("Cheese",35),
+("Butter",20),
+("Butter Cheese",50);
+
+select * from food_item;
+select * from variations;
+
+-- cross join
+/*
+Cartesian Join -> combines all rows from one table
+into another table
+It doesn't require any conditions
+*/
+
+-- menu
+
+select * from food_item
+cross join variations;
+
+
+select concat(toppings," ", name) as menu,
+price + extra_amount as amount
+from food_item
+cross join variations
+order by name, amount;
+
