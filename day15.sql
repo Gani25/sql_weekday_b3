107000958eb5199a4e6278e2e6154c87ab59378d
Gani25 <memonabdulgani3d@gmail.com>
2025-11-12 10:58:19 +0530

    Create day15.sql

---
+++ day15.sql
@@ -0,0 +1,57 @@
+-- subquery day 15
+
+use sprk_morning;
+
+select * from employee;
+
+-- find all employees who earns highest salary in their department
+
+-- find max salary in each department
+select distinct dept_id from employee;
+select dept_id, max(salary) from employee
+group by dept_id;
+
+
+select * from employee;
+/*
+1	6000
+2	10000
+3	10000
+4	9500
+5	7700
+8	1800
+11	1500
+*/
+
+select * from employee
+where dept_id = 1 and salary = 6000;
+
+select * from employee
+where (dept_id, salary) in 
+	(
+		select dept_id, max(salary) from employee
+        group by dept_id
+	);
+    select * from employee;
+-- subquery in from clause
+
+select * from employee;
+-- find all employee who are with us for past 5 years
+
+-- year of experience
+-- current year - joining year
+
+/*
+Error Code: 1054. Unknown column 'experience' in 'where clause'
+
+*/
+select *, year(now()) - year(joining_date) as experience 
+from employee;
+
+select * from 
+	(
+		select *, year(now()) - year(joining_date) as experience 
+		from employee
+    ) as emp_with_experience
+where experience >= 5;
+
