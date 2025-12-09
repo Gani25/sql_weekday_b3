f93e4f5a579c156056274d36c9a611c2c0866fb4
Gani25 <memonabdulgani3d@gmail.com>
2025-12-09 11:41:27 +0530

    Update day28.sql

--- day28.sql
+++ day28.sql
@@ -86,3 +86,85 @@ with emp_dept_rank as
 )
 select * from emp_dept_rank
 where emp_salary_rank = 1;
+
+-- value functions
+/*
+lead()
+lag()
+first_value()
+last_value()
+nth_value()
+*/
+
+-- New Tables
+
+
+create table train_schedule
+(
+	train_id int,
+    station varchar(100),
+    train_time time
+);
+
+truncate table train_schedule;
+insert into train_schedule values
+(102,"Kalyan","10:20:00"),
+(101,"Vashi","7:50:00"),
+(101,"CST","8:45:00"),
+(101,"Panvel","7:15:00"),
+(102,"CST","11:55:00"),
+(101,"Kharghar","7:30:00"),
+(102,"Thane","10:50:00"),
+(102,"Kurla","11:05:00"),
+(102,"Byculla","11:46:00"),
+(101,"Kurla","8:10:00"),
+(103,"Virar","14:03:00"),
+(103,"Churchgate","12:10:00"),
+(103,"Andheri","13:05:00"),
+(103,"Dadar","12:30:00");
+
+
+select * from train_schedule;
+select *, 
+ifnull(
+	lead(station) over(partition by train_id order by train_time)
+    ,"End Of Train"
+)
+next_station 
+from train_schedule;
+
+
+select *, 
+ifnull(
+	lead(station) over(partition by train_id order by train_time)
+    ,"End Of Train"
+)
+next_station,
+ifnull(
+	lead(train_time) over(partition by train_id order by train_time)
+    ,"END"
+)
+next_station_time,
+ifnull(
+	lag(station) over(partition by train_id order by train_time)
+    ,"Start Of Train"
+)
+prev_station,
+ifnull(
+	lag(train_time) over(partition by train_id order by train_time)
+    ,"START"
+)
+prev_station_time
+
+from train_schedule;
+
+select * from train_schedule;
+select *, first_value(station) over(partition by train_id
+order by train_time) 
+as start_station
+from train_schedule;
+
+select *, nth_value(station,2) over(partition by train_id
+order by train_time) 
+as start_station
+from train_schedule;
