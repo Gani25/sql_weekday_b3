ad4eced6f0d890af398656e9cf4c9b36b42c88ec
Gani25 <memonabdulgani3d@gmail.com>
2025-12-15 11:45:04 +0530

    Update day32.sql

--- day32.sql
+++ day32.sql
@@ -86,4 +86,80 @@ json_object(
 )
 );
 
+select *, details ->"$.age" from employee;
+select *, details ->"$.skills" from employee;
+select *, details ->"$.skills[0]" from employee;
+select *, details ->"$.address" from employee;
+select *, details ->"$.address.pincode" from employee;
+
+
+select *, Json_extract(details,"$.age")  from employee;
+select *, Json_extract(details,"$.skills")  from employee;
+select *, Json_extract(details,"$.skills[0]")  from employee;
+
+select *, Json_extract(details,"$.address.pincode")  from employee;
+
+-- where age is 30
+select * from employee
+where details->"$.age" = 30;
+
+select * from employee
+where details->"$.age" < 30;
+
+-- update
+
+select * from employee;
+
+update employee
+set details = json_set(details, "$.age",35)
+where emp_id = 3;
+
+select * from employee;
+
+update employee
+set details = json_set(details, "$.skills",json_array("Python","Excel","PowerBI"))
+where emp_id = 1;
+
+select * from employee;
+
+update employee
+set details = json_set(details, "$.skills[1]","Java")
+where emp_id = 1;
+
+select * from employee;
+
+update employee
+set details = json_array_append(details, "$.skills","Spring Boot")
+where emp_id = 1;
+
+select * from employee;
+
+update employee
+set details = json_remove(details, "$.skills")
+where emp_id = 1;
+
+select * from employee;
+
+update employee
+set details = json_remove(details, "$.address.pincode")
+where emp_id = 2;
+
+select * from employee;
+
+update employee
+set details = '{}'
+where emp_id = 1;
+
+select * from employee;
+
+update employee
+set details = Json_object()
+where emp_id = 2;
+
+
+select * from employee;
+
+delete from employee
+where details -> "$.age" = 35;
+
 select * from employee;
